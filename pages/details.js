45e724e09b106380a90e62f25e7aba1b3ac8470e
chenmingzhen <214164051@qq.com>
2020-09-03 18:43:45 +0800

    refactor:⚒use marked and highlight.js

--- pages/details.js
+++ pages/details.js
@@ -1,52 +1,36 @@
 import React from "react";
 import Head from "next/head";
-import { Row, Col, Breadcrumb, Affix } from "antd";
+import { Affix, Breadcrumb, Col, Row } from "antd";
 import Header from "../components/Header";
 import Author from "../components/Author";
 import Footer from "../components/Footer";
 import Icon from "../components/Icon";
-import ReactMarkdown from "react-markdown";
 import MarkNav from "markdown-navbar";
+import axios from "axios";
 import "markdown-navbar/dist/navbar.css";
 import "../static/style/pages/detail.scss";
+import marked from "marked";
+import hljs from "highlight.js";
+import "highlight.js/styles/monokai-sublime.css";
 
-const Detailed = () => {
-  let markdown =
-    "# P01:课程介绍和环境搭建\n" +
-    "[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n" +
-    "> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n" +
-    "**这是加粗的文字**\n\n" +
-    "*这是倾斜的文字*`\n\n" +
-    "***这是斜体加粗的文字***\n\n" +
-    "~~这是加删除线的文字~~ \n\n" +
-    "`console.log(111)` \n\n" +
-    "# p02:来个Hello World 初始Vue3.0\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n" +
-    "***\n\n\n" +
-    "# p03:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "# p04:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "#5 p05:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "# p06:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "# p07:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "``` var a=11; ```";
+const Detailed = ({ article_content }) => {
+  const renderer = new marked.Renderer();
 
+  marked.setOptions({
+    renderer:renderer,
+    gfm:true,
+    pedantic:false,
+    sanitize:false,
+    tables:true,
+    breaks:false,
+    smartLists:true,
+    highlight:function(code){
+      return hljs.highlightAuto(code).value
+    }
+  })
+
+  let html = marked(article_content);
+  console.log(html);
   return (
     <>
       <Head>
@@ -75,9 +59,7 @@ const Detailed = () => {
             </div>
 
             <div>
-              <div className="detailed-title">
-                Blog开发
-              </div>
+              <div className="detailed-title">Blog开发</div>
               <div className="list-icon center">
                 <span className="list-item">
                   <Icon type="Calender" />
@@ -92,10 +74,10 @@ const Detailed = () => {
                   <div className="text">1次</div>
                 </span>
               </div>
-
-              <div className="detailed-content">
-                <ReactMarkdown source={markdown} escapeHtml={false} />
-              </div>
+              <div
+                className="detailed-content"
+                dangerouslySetInnerHTML={{ __html: html }}
+              />
             </div>
           </div>
         </Col>
@@ -105,11 +87,7 @@ const Detailed = () => {
           <Affix offsetTop={30}>
             <div className="detailed-nav comm-box">
               <div className="nav-title">文章目录</div>
-              <MarkNav
-                className="article-menu"
-                source={markdown}
-                ordered={false}
-              />
+              <MarkNav className="article-menu" source={html} ordered={false} />
             </div>
           </Affix>
         </Col>
@@ -117,6 +95,17 @@ const Detailed = () => {
       <Footer />
     </>
   );
+};
+
+Detailed.getInitialProps = async (context) => {
+  const id = context.query.id;
+  const promise = new Promise((resolve) => {
+    axios("http://127.0.0.1:7001/default/getArticleById/" + id).then((res) => {
+      resolve(res.data.data[0]);
+    });
+  });
+
+  return await promise;
 };
 
 export default Detailed;
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,76 @@
 import Head from "next/head";
 import Header from "../components/Header";
-import { Row, Col, List } from "antd";
-import React, { useState } from "react";
+import {Row, Col, List} from "antd";
+import React, {useState} from "react";
 import Author from "../components/Author";
 import Footer from "../components/Footer";
 import Icon from "../components/Icon";
+import axios from 'axios';
+import Link from 'next/link'
 import "../static/style/pages/index.scss";
 
 
-export default function Home() {
-  const [mylist, setMylist] = useState(
-    [
-      {
-        title: "理解和使用ES6中的Symbol",
-        context:
-          "掌握程序人的学习方法。 也许你刚步入IT行业，也许你遇到了成长瓶颈，也许你不知道该学习什么知识，也许你不会融入团队，也许...........有些时候你陷入彷徨。 你需要一个强力的队友，你需要一个资深老手，你需要一个随时可以帮助你的人，你更需要一个陪你加速前行的。 我在这个行业走了12年，从后端、前端到移动端都从事过，从中走了很多坑，但我有一套适合程序员的学习方法。 如果你愿意，我将带着你在这个程序行业加速奔跑。分享我学习的方法，所学的内容和一切我的资料。 你遇到的职业问题，我也会第一时间给你解答。我需要先感谢一直帮助我的小伙伴，这个博客能产出300多集免费视频，其中有他们的鼎力支持，如果没有他们的支持和鼓励，我可能早都放弃了。 原来我博客只是录制免费视频，然后求30元的打赏。 每次打赏我都会觉得内疚，因为我并没有给你特殊的照顾，也没能从实质上帮助过你。 直到朋友给我介绍了知识星球，它可以专享加入，可以分享知识，可以解答问题，所以我如获珍宝，决定把打赏环节改为知识服务。我定价50元每年，为什么是50元每年？因为这是知识星球允许的最低收费了。",
-      },
-      {
-        title: "理解和使用ES6中的Symbol",
-        context:
-          "掌握程序人的学习方法。 也许你刚步入IT行业，也许你遇到了成长瓶颈，也许你不知道该学习什么知识，也许你不会融入团队，也许...........有些时候你陷入彷徨。 你需要一个强力的队友，你需要一个资深老手，你需要一个随时可以帮助你的人，你更需要一个陪你加速前行的。 我在这个行业走了12年，从后端、前端到移动端都从事过，从中走了很多坑，但我有一套适合程序员的学习方法。 如果你愿意，我将带着你在这个程序行业加速奔跑。分享我学习的方法，所学的内容和一切我的资料。 你遇到的职业问题，我也会第一时间给你解答。我需要先感谢一直帮助我的小伙伴，这个博客能产出300多集免费视频，其中有他们的鼎力支持，如果没有他们的支持和鼓励，我可能早都放弃了。 原来我博客只是录制免费视频，然后求30元的打赏。 每次打赏我都会觉得内疚，因为我并没有给你特殊的照顾，也没能从实质上帮助过你。 直到朋友给我介绍了知识星球，它可以专享加入，可以分享知识，可以解答问题，所以我如获珍宝，决定把打赏环节改为知识服务。我定价50元每年，为什么是50元每年？因为这是知识星球允许的最低收费了。",
-      },
-      {
-        title: "理解和使用ES6中的Symbol",
-        context:
-          "掌握程序人的学习方法。 也许你刚步入IT行业，也许你遇到了成长瓶颈，也许你不知道该学习什么知识，也许你不会融入团队，也许...........有些时候你陷入彷徨。 你需要一个强力的队友，你需要一个资深老手，你需要一个随时可以帮助你的人，你更需要一个陪你加速前行的。 我在这个行业走了12年，从后端、前端到移动端都从事过，从中走了很多坑，但我有一套适合程序员的学习方法。 如果你愿意，我将带着你在这个程序行业加速奔跑。分享我学习的方法，所学的内容和一切我的资料。 你遇到的职业问题，我也会第一时间给你解答。我需要先感谢一直帮助我的小伙伴，这个博客能产出300多集免费视频，其中有他们的鼎力支持，如果没有他们的支持和鼓励，我可能早都放弃了。 原来我博客只是录制免费视频，然后求30元的打赏。 每次打赏我都会觉得内疚，因为我并没有给你特殊的照顾，也没能从实质上帮助过你。 直到朋友给我介绍了知识星球，它可以专享加入，可以分享知识，可以解答问题，所以我如获珍宝，决定把打赏环节改为知识服务。我定价50元每年，为什么是50元每年？因为这是知识星球允许的最低收费了。",
-      },
-      {
-        title: "理解和使用ES6中的Symbol",
-        context:
-          "掌握程序人的学习方法。 也许你刚步入IT行业，也许你遇到了成长瓶颈，也许你不知道该学习什么知识，也许你不会融入团队，也许...........有些时候你陷入彷徨。 你需要一个强力的队友，你需要一个资深老手，你需要一个随时可以帮助你的人，你更需要一个陪你加速前行的。 我在这个行业走了12年，从后端、前端到移动端都从事过，从中走了很多坑，但我有一套适合程序员的学习方法。 如果你愿意，我将带着你在这个程序行业加速奔跑。分享我学习的方法，所学的内容和一切我的资料。 你遇到的职业问题，我也会第一时间给你解答。我需要先感谢一直帮助我的小伙伴，这个博客能产出300多集免费视频，其中有他们的鼎力支持，如果没有他们的支持和鼓励，我可能早都放弃了。 原来我博客只是录制免费视频，然后求30元的打赏。 每次打赏我都会觉得内疚，因为我并没有给你特殊的照顾，也没能从实质上帮助过你。 直到朋友给我介绍了知识星球，它可以专享加入，可以分享知识，可以解答问题，所以我如获珍宝，决定把打赏环节改为知识服务。我定价50元每年，为什么是50元每年？因为这是知识星球允许的最低收费了。",
-      },
-    ],
-    []
-  );
-  return (
-    <div>
-      <Head>
-        <title>Home</title>
-      </Head>
-      <Header />
+export default function Home({data}) {
+    const [mylist, setMylist] = useState(data,
+        []
+    );
+    return (
+        <div>
+            <Head>
+                <title>Home</title>
+            </Head>
+            <Header/>
 
-      <Row className="comm-main" type="flex" justify="center">
-        <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
-          <div>
-            <List
-              header={<div>最新日志</div>}
-              itemLayout="vertical"
-              dataSource={mylist}
-              renderItem={(item) => (
-                <List.Item>
-                  <div className="list-title">{item.title}</div>
-                  <div className="list-icon">
-                    <span className="list-item">
-                      <Icon type="Calender" />
-                      <div className="text">2020-9-2</div>
-                    </span>
-                    <span className="list-item">
-                      <Icon type="Folder" />
-                      <div className="text">技术</div>
-                    </span>
-                    <span className="list-item">
-                      <Icon type="Fire" />
-                      <div className="text">1次</div>
-                    </span>
-                  </div>
-                  <div className="list-context">{item.context}</div>
-                </List.Item>
-              )}
-            />
-          </div>
-        </Col>
+            <Row className="comm-main" type="flex" justify="center">
+                <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
+                    <div>
+                        <List
+                            header={<div>最新日志</div>}
+                            itemLayout="vertical"
+                            dataSource={mylist}
+                            renderItem={(item) => (
+                                <List.Item>
+                                    <div className="list-title"><Link
+                                        href={{pathname: '/details', query: {id: item.id}}}><a>{item.title}</a></Link>
+                                    </div>
+                                    <div className="list-icon">
+                                        <span className="list-item">
+                                          <Icon type="Calender"/>
+                                          <div className="text">{item.addTime}</div>
+                                        </span>
+                                                            <span className="list-item">
+                                          <Icon type="Folder"/>
+                                          <div className="text"> {item.typeName}</div>
+                                        </span>
+                                                            <span className="list-item">
+                                          <Icon type="Fire"/>
+                                          <div className="text">{item.view_count}次</div>
+                                        </span>
+                                    </div>
+                                    <div className="list-context">{item.introduce}</div>
+                                </List.Item>
+                            )}
+                        />
+                    </div>
+                </Col>
 
-        <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
-          <Author />
-        </Col>
-      </Row>
-      <Footer />
-    </div>
-  );
+                <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+                    <Author/>
+                </Col>
+            </Row>
+            <Footer/>
+        </div>
+    );
+}
+
+Home.getInitialProps = async () => {
+    const promise = new Promise((resolve) => {
+        axios('http://127.0.0.1:7001/default/getArticleList').then(
+            (res) => {
+                console.log('远程获取数据结果:', res.data)
+                resolve(res.data)
+            }
+        )
+    })
+    return await promise
 }
